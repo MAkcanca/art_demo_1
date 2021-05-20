391501115108a43d5f23e03feff37ac2f4d68f23
Mustafa Akcanca <MAkcanca@users.noreply.github.com>
2021-05-20 22:11:39 +0300

    Backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Video = (params) => {
+
+  return <video src={params.videoUrl}></video>
 }
 
-export default App;
+
+export default function App() {
+  const [videoUrl, setVideo] = useState('');
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+    console.log(result)
+  }, [result])
+
+  return (
+    <>
+      <Video videoUrl={videoUrl} />
+      <Search handleResult={setResult} />
+    </>
+  )
+}
